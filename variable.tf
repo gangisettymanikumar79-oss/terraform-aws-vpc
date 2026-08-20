e1f8ac926764ae071ec733b86aec72bbc56af01f
.email.com <gangisettymani98@gmail.com>
2026-08-20 22:33:35 +0530

    terraform vpc-test

--- variable.tf
+++ variable.tf
@@ -9,6 +9,7 @@ variable "environment" {
 variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
+    
   
 }
 variable "vpc_tags" {
@@ -86,6 +87,7 @@ variable "is_peering_required" {
 variable "vpc_peering_tags" {
     default = {}
     type = map
-  
 }
+
+
 
